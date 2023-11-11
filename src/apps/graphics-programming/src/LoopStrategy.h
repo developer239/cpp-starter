7422c07737e35a538c5f486e0fc2b7552878cf73
michaljarnot <m.jarnot@yahoo.com>
2023-11-11 22:53:43 +0100

    feat: minimal imgui implementation

--- src/apps/graphics-programming/src/LoopStrategy.h
+++ src/apps/graphics-programming/src/LoopStrategy.h
@@ -3,13 +3,18 @@
 #include "core/AssetStore.h"
 #include "core/IStrategy.h"
 #include "core/Window.h"
+#include "geometry/Cube.h"
+#include "gui/Debug.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_sdlrenderer2.h"
 #include "logger/Logger.h"
 #include "rendering/Painter.h"
 #include "rendering/WavefrontLoader.h"
-#include "geometry/Cube.h"
 
 class LoopStrategy : public Core::IStrategy {
  public:
+  Debug debug;
+
   std::shared_ptr<Cube> cube;
   std::shared_ptr<bool> shouldRotate;
   std::shared_ptr<bool> shouldCull;
@@ -24,6 +29,20 @@ class LoopStrategy : public Core::IStrategy {
     shouldCull = std::make_shared<bool>(false);
 
     WavefrontLoader::LoadObjFile("assets/models/f22.obj", *cube);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForSDLRenderer(window.Get().get(), renderer.Get().get());
+    ImGui_ImplSDLRenderer2_Init(renderer.Get().get());
   }
 
   void HandleEvent(SDL_Event& event) override {}
@@ -41,6 +60,7 @@ class LoopStrategy : public Core::IStrategy {
   }
 
   void OnRender(Core::Window& window, Core::Renderer& renderer) override {
-    painter.RenderColorBuffer(renderer);
+    //    painter.RenderColorBuffer(renderer);
+        debug.Render(*shouldRotate, *shouldCull);
   }
 };
--- src/apps/graphics-programming/src/main.cpp
+++ src/apps/graphics-programming/src/main.cpp
@@ -1,7 +1,12 @@
 #include "LoopStrategy.h"
+#include "core-imgui/Strategy.h"
 #include "core/Loop.h"
 
 int main() {
+  // TODO: uncomment strategy
+  // TODO: step by step move stuff from loopstrategy init to gui strategy
+  // TODO: step by step move stuff from Debug to gui strategy
+//  CoreImGui::Strategy gui;
   LoopStrategy strategy;
 
   auto loop = Core::Loop({&strategy});
